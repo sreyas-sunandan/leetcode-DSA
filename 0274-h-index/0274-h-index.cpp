d8544f4d091f0f7457bdbd2495f0030729a1047d
Sreyas Sunandan <144914489+sreyas-sunandan@users.noreply.github.com>
2025-02-23 12:13:23 +0530

    Time: 0 ms (100%), Space: 12.9 MB (20.72%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        float sum = 0;
-        float avg = 0;
-        int cit = 0;
-        for (int i = 0; i < citations.size(); i++){
-            sum += citations[i];
+        int papers = citations.size();
+        vector<int> citbucket(papers + 1,0);
+        for (int cit : citations){
+            citbucket[min(cit,papers)]++;
         }
-        if (sum == 0) return 0;
-        avg = sum / citations.size();
-        if (sum == citations.size()) return avg;
-         for (int i = 0; i < citations.size(); i++){
-           if (citations[i] >= avg) cit++;
+        int cumulative = 0;
+         for (int i = papers; i >= 0; i--){
+            cumulative += citbucket[i];
+            if (cumulative >= i) return i;
         }
-        return cit;
+        return 0;
     }
 };
