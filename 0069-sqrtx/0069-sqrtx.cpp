10f801a49b2f802fbbfefb0c1c3d6b720fe888a6
Sreyas Sunandan <144914489+sreyas-sunandan@users.noreply.github.com>
2025-12-25 07:50:58 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.97%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+    if (x < 2) return x;
+
+    int low = 1, high = x / 2, ans = 0;
+
+    while (low <= high) {
+        long mid = low + (high - low) / 2;
+        if (mid * mid == x)
+            return mid;
+        else if (mid * mid < x) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return ans;
+}
+
+};
